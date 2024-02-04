224bd2ea4ef836d8eb4fad22443163f690a54626
naoyuki.takayanagi <naoyuki.takayanagi@medley.jp>
2024-02-04 23:51:01 +0900

    2.3.2

---
+++ dist/section2/2.3.2.js
@@ -0,0 +1,7 @@
+"use strict";
+const width1 = 5;
+const width2 = 8;
+const height = 3;
+const area = (width1 + width2) * height / 2;
+console.log(area);
+console.log(3.5 * 2);
---
+++ src/section2/2.3.2.ts
@@ -0,0 +1,7 @@
+const width1 = 5;
+const width2 = 8;
+const height: number = 3;
+const area = (width1 + width2) * height / 2;
+console.log(area);
+
+console.log(3.5 * 2);
