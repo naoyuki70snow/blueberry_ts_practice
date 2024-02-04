37ed18f67b3a7e05796a658faa0834fedf2bad1e
naoyuki.takayanagi <naoyuki.takayanagi@medley.jp>
2024-02-04 23:53:35 +0900

    2.3.3

---
+++ dist/section2/2.3.3.js
@@ -0,0 +1,10 @@
+"use strict";
+const binary = 0b1010;
+const octal = 0o755;
+const hexadecimal = 0xff;
+console.log(binary, octal, hexadecimal);
+const big = 1e8;
+const small = 4e-5;
+console.log(big, small);
+const million = 1000000;
+console.log(million);
