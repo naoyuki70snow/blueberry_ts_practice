167ea20d0df5dbe78607015225b5e2c719a79216
naoyuki.takayanagi <naoyuki.takayanagi@medley.jp>
2024-02-07 00:38:02 +0900

    2.4.3

---
+++ src/section2/2.4.3.ts
@@ -0,0 +1,14 @@
+// import { createInterface } from 'readline';
+
+// const rl = createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// rl.question('名前を入力してください: ', (name: string) => {
+//   console.log('こんにちは、' + name + 'さん');
+//   rl.close();
+// });
+
+console.log("foo" + true);
+console.log(null + "bar");
