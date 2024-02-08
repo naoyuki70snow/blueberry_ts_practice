80db203c6a9d3688f137dba486d81833088bc35e
naoyuki.takayanagi <naoyuki.takayanagi@medley.jp>
2024-02-09 00:15:49 +0900

    2.4.8

---
+++ dist/section2/2.4.8.js
@@ -0,0 +1,20 @@
+"use strict";
+// import { createInterface } from 'readline';
+// const rl = createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+// rl.question('名前を入力して下さい:', (name) => {
+//   if (name === '') {
+//     name = "名無し";
+//   }
+//   console.log(`こんにちは、${name}さん`);
+//   rl.close();
+// });
+// let num = 0;
+// num += 100;
+// num *= 4;
+// num -= 200;
+// num /= 2;
+// num **= 0.5;
+// console.log(num);
