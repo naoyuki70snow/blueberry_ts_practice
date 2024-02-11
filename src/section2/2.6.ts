f7a44cbfd35b273ba6cee901c2f9f6eb31469995
naoyuki.takayanagi <naoyuki.takayanagi@medley.jp>
2024-02-11 16:41:01 +0900

    2.6.1_力試し

--- src/section2/2.6.ts
+++ src/section2/2.6.ts
@@ -1,14 +1,19 @@
+// let result: string = '';
 // for(let i=1; i <= 100; i++) {
+//   if (i > 1) {
+//     result += ' ';
+//   }
 //   const div3 = i % 3 === 0;
 //   const div5 = i % 5 === 0;
 
 //   if (div3 && div5) {
-//     console.log('FizzBuzz');
+//     result += 'FizzBuzz';
 //   } else if (div3) {
-//     console.log('Fizz');
+//     result += 'Fizz';
 //   } else if (div5) {
-//     console.log('Buzz');
+//     result += 'Buzz';
 //   } else {
-//     console.log(i);
+//     result += String(i);
 //   }
 // }
+// console.log(result);
