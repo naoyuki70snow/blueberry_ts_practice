6d98863caf1ffcd3b967c6c15844b17455d21b0e
naoyuki.takayanagi <naoyuki.takayanagi@medley.jp>
2024-02-11 16:36:14 +0900

    2.6.1

---
+++ src/section2/2.6.1.ts
@@ -0,0 +1,14 @@
+// for(let i=1; i <= 100; i++) {
+//   const div3 = i % 3 === 0;
+//   const div5 = i % 5 === 0;
+
+//   if (div3 && div5) {
+//     console.log('FizzBuzz');
+//   } else if (div3) {
+//     console.log('Fizz');
+//   } else if (div5) {
+//     console.log('Buzz');
+//   } else {
+//     console.log(i);
+//   }
+// }
