8ededf84dc14c7f91df66a6703debbf747ed0cc2
naoyuki.takayanagi <naoyuki.takayanagi@medley.jp>
2024-02-14 00:09:51 +0900

    3.1.3まで

---
+++ src/section3/3.1.ts
@@ -0,0 +1,52 @@
+// 3.1.1
+// const obj = {
+//   foo: 123,
+//   bar: "Hello, world!"
+// }
+
+// console.log(obj.foo);
+// console.log(obj.bar);
+
+// 3.1.2
+// const obj = {
+//   foo: 555,
+//   bar: "文字列",
+// }
+// const user = {
+//   name: input ? input : "名無し",
+//   age: 20,
+// }
+// const name = input ? input : "名無し";
+// const user = {
+//   name: name,
+//   age: 20,
+// }
+// const name = input ? input : "名無し";
+// const user = {
+//   name,
+//   age: 20,
+// }
+
+// 3.1.3
+// const obj = {
+//   "foo": 123,
+//   "foo bar": -500,
+//   '↑↓↑↓': ""
+// }
+
+// console.log(obj.foo);
+// console.log(obj["foo bar"]);
+
+// const obj = {
+//   1: "one",
+//   2.05: "two point o five",
+// }
+
+// console.log(obj["1"]);
+// console.log(obj["2.05"]);
+
+// const propName = "foo";
+// const obj = {
+//   [propName]: 123
+// };
+// console.log(obj.foo);
