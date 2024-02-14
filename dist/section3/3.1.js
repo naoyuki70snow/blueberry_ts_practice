a71157c0f49c22c12b33c3b82d2d70e6a59ae834
naoyuki.takayanagi <naoyuki.takayanagi@medley.jp>
2024-02-15 02:05:53 +0900

    3.1.4

--- dist/section3/3.1.js
+++ dist/section3/3.1.js
@@ -44,3 +44,24 @@
 //   [propName]: 123
 // };
 // console.log(obj.foo);
+// 3.1.4
+// const user = {
+//   name: "uhyo",
+//   age: 25,
+// }
+// user.age = 26;
+// console.log(user.age);
+// import { createInterface } from "readline";
+// const rl = createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+// const messages = {
+//   good: "0以上の数値が入力されました！",
+//   bad: "負の数値を入力しないでください！",
+// }
+// rl.question("数値を入力してください:", (line) => {
+//   const num = Number(line);
+//   console.log(messages[num >= 0 ? "good" : "bad"]);
+//   rl.close();
+// });
