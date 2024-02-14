45810714c9289457ed7868ff3651d883eaf5a0b6
naoyuki.takayanagi <naoyuki.takayanagi@medley.jp>
2024-02-15 02:11:39 +0900

    3.1.5

--- dist/section3/3.1.js
+++ dist/section3/3.1.js
@@ -65,3 +65,36 @@
 //   console.log(messages[num >= 0 ? "good" : "bad"]);
 //   rl.close();
 // });
+// 3.1.5
+// const obj1 = {
+//   bar: 456,
+//   baz: 789,
+// }
+// const obj2 = {
+//   foo: 123,
+//   ...obj1,
+// }
+// console.log(obj2);
+// const obj1 = {
+//   foo: 123,
+//   bar: 456,
+//   baz: 789,
+// }
+// const obj2 = {
+//   ...obj1,
+//   foo: -9999,
+// }
+// console.log(obj2);
+// const obj1 = {
+//   foo: 123,
+//   bar: 456,
+// }
+// const obj2 = {
+//   bar: -999,
+//   baz: -9999,
+// }
+// const obj3 = {
+//   ...obj1,
+//   ...obj2,
+// }
+// console.log(obj3);
