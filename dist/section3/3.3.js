afa521ef29dd989fa8b7fcef188701d0ad19d086
naoyuki.takayanagi <naoyuki.takayanagi@medley.jp>
2024-02-18 14:10:28 +0900

    3.3

---
+++ dist/section3/3.3.js
@@ -0,0 +1,51 @@
+"use strict";
+// 3.3
+// 3.3.1
+// type FooBar = {
+//   foo: string;
+//   bar: number;
+// }
+// type FooBarBaz = {
+//   foo: string;
+//   bar: number;
+//   baz: boolean;
+// }
+// const obj: FooBarBaz = {
+//   foo: "hi",
+//   bar: 1,
+//   baz: false,
+// }
+// const obj2: FooBar = obj;
+// 3.3.2
+// type Anial = {
+//   age: number;
+// }
+// type Human = {
+//   age: number;
+//   name: string;
+// }
+// type AnimalFamily = {
+//   familyName: string;
+//   mother: Anial;
+//   father: Anial;
+//   child: Anial;
+// }
+// type HumanFamily = {
+//   familyName: string;
+//   mother: Human;
+//   father: Human;
+//   child: Human;
+// }
+// 3.3.3
+// type User = { name: string; age: number; }
+// const u: User = {
+//   name: "uhyo",
+//   age: 26,
+//   telNumber: "09012345678"
+// }
+// const obj = {
+//   name: "uhyo",
+//   age: 26,
+//   telNumber: "09012345678"
+// }
+// const u: User = obj;
